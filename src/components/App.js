bbd3128ab3ef634459242f43459b8ea6c647dce7
aziza-iliasova <miacorporation25@gmail.com>
2021-07-05 18:24:34 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import About from "../components/About";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
 
 function App() {
   return (
+   
     <div>
       <NavBar />
       <Home />
